b65d437b9b11f48e7bd3f6fb9ff10ca3ec4a116f
Ahmet Simsek <iamaroott@gmail.com>
2017-06-15 19:50:57 +0300

    0.0.1

--- bin/index.js
+++ bin/index.js
@@ -1,13 +1,15 @@
+#! /usr/bin/env node
+
 const { exec } = require('child_process')
 
-exec(`sh ${process.cwd()}/replace.sh ${process.argv[2]}`, () => {
+exec(`sh ${process.cwd()}/replace.sh ${process.argv.slice(2).join('+')}`, () => {
 
   exec(`sh ${process.cwd()}/curl.sh`, (error, stdout, stderr) => {
     stdout = JSON.parse(stdout)
 
     let images = decodeURIComponent(encodeURIComponent(stdout[1][1])).match(/imgurl?=(.*?)&amp;/g)
 
-    console.log(images.map(image => image.slice(7).substr(0, image.length-12)))
+    console.log(images.map(image => image.slice(7).substr(0, image.length-12).split('%')[0]))
 
   })
 
---
+++ test.js
@@ -0,0 +1,12 @@
+import test from 'ava';
+import imagefinder from './lib/index';
+
+test('#1', async t => {
+
+  let images = await imagefinder({
+    keyword: 'javascript'
+  })
+
+  t.is(images.length, 100)
+
+})
