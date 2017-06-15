13764d62905a361da2df13aa64e10e6bde5941b7
Ahmet Simsek <iamaroott@gmail.com>
2017-06-15 21:29:18 +0300

    0.0.5

--- bin/index.js
+++ bin/index.js
@@ -3,14 +3,16 @@
 const { exec } = require('child_process')
 
 exec(`sh ${__dirname}/../replace.sh ${process.argv.slice(2).join('+')}`, () => {
-
-  exec(`sh ${__dirname}/../curl.sh`, (error, stdout, stderr) => {
+  
+  exec(`sh ${process.cwd()}/curl.sh`, (error, stdout, stderr) => {
     
     stdout = JSON.parse(stdout)
 
     let images = decodeURIComponent(encodeURIComponent(stdout[1][1])).match(/imgurl?=(.*?)&amp;/g)
 
     console.log(images.map(image => image.slice(7).substr(0, image.length-12).split('%')[0]))
+    
+    exec(`rm ${process.cwd()}/curl.sh`)
 
   })
 
