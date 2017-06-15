5a12184c94dad326dfb18f15d060b4074e89bc14
Ahmet Simsek <iamaroott@gmail.com>
2017-06-15 21:36:34 +0300

    bash

--- bin/index.js
+++ bin/index.js
@@ -2,9 +2,9 @@
 
 const { exec } = require('child_process')
 
-exec(`sh ${__dirname}/../replace.sh ${process.argv.slice(2).join('+')}`, () => {
+exec(`bash ${__dirname}/../replace.sh ${process.argv.slice(2).join('+')}`, () => {
   
-  exec(`sh ${process.cwd()}/curl.sh`, (error, stdout, stderr) => {
+  exec(`bash ${process.cwd()}/curl.sh`, (error, stdout, stderr) => {
     
     stdout = JSON.parse(stdout)
 
