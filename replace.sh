94703b71e83f3b9ec165704af1ee62afa0cd3700
Ahmet Simsek <iamaroott@gmail.com>
2017-08-19 16:48:55 +0300

    .

--- replace.sh
+++ replace.sh
@@ -1,4 +1,4 @@
-run="curl https://www.google.com/search\?ei\=mZ1CWb-EDcOz-AGX-I6QBQ\&yv\=2\&q\=#\&vet\=10ahUKEwj_tIzKi8DUAhXDGT4KHRe8A1IQuT0IOSgB.mZ1CWb-EDcOz-AGX-I6QBQ.i\&ved\=0ahUKEwj_tIzKi8DUAhXDGT4KHRe8A1IQuT0IOSgB\&ijn\=1\&start\=100\&asearch\=ichunk\&async\=_id:rg_s,_pms:s"
+run="curl https://www.google.com/search\?ei\=mZ1CWb-EDcOz-AGX-I6QBQ\&yv\=2\&q\=#\&vet\=10ahUKEwj_tIzKi8DUAhXDGT4KHRe8A1IQuT0IOSgB.mZ1CWb-EDcOz-AGX-I6QBQ.i\&ved\=0ahUKEwj_tIzKi8DUAhXDGT4KHRe8A1IQuT0IOSgB\&ijn\=1\&start\=0\&asearch\=ichunk\&async\=_id:rg_s,_pms:s"
 run=${run/\#/$1}
 echo "" > $PWD/curl.sh
 echo $run > $PWD/curl.sh
